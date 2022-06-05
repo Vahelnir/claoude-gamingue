0a37930d64167d115a93c4ca5b3056f0d31b5551
Vahelnir <vahelnir@gmail.com>
2022-06-05 19:08:01 +0200

    add mysql connector

--- web-server/base_skeleton.sql
+++ web-server/base_skeleton.sql
@@ -7,5 +7,3 @@ alter table `vmcache` add unique `vmcache_resource_id_unique`(`resource_id`);
 create table `user` (`id` varchar(255) not null, `email` varchar(255) not null, `password` varchar(255) not null, `subscribed` tinyint(1) not null, `created_at` datetime not null, `edited_at` datetime not null, primary key (`id`)) default character set utf8mb4 engine = InnoDB;
 
 set foreign_key_checks = 1;
-
-[32mSchema successfully created[39m
